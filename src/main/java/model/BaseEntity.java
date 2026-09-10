793b3547bbd4ed9e1a942704635270dbbcdb5f53
Behradjahangiri <behrad.jager@gmail.com>
2026-09-10 14:24:49 +0330

    BaseEntity fix for hibernate

--- src/main/java/model/BaseEntity.java
+++ src/main/java/model/BaseEntity.java
@@ -3,11 +3,19 @@ package model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @SuperBuilder
+@ToString
+
 public abstract class BaseEntity {
+    private boolean deleted;
 }
