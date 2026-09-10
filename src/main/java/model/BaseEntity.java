e0132a94883dbb24954ac997257a706e2e7ebbb8
Behradjahangiri <behrad.jager@gmail.com>
2026-09-10 13:29:14 +0330

    persistence configurations hibernate jpa DB oracle DB H2

--- src/main/java/model/BaseEntity.java
+++ src/main/java/model/BaseEntity.java
@@ -1,4 +1,13 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
 public abstract class BaseEntity {
 }
