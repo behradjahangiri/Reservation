a42f7559cd70dde346f4b74f2d5fc01ead44f404
Behradjahangiri <behrad.jager@gmail.com>
2026-09-10 14:34:25 +0330

    Day entity

--- src/main/java/model/Day.java
+++ src/main/java/model/Day.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Day {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuperBuilder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table
+
+public class Day extends BaseEntity {
+    @Id
+    private Integer id;
+    private DayOfWeek dayOfWeek;
+    private int classNumbers = 0;
+    private List<ClassInfo> classList = new ArrayList<>();
+
 }
