1b94364e63301b5ca7b470a014056257d943b846
Behradjahangiri <behrad.jager@gmail.com>
2026-09-11 17:51:44 +0330

    Create DayDto Create ClassInfoDto

---
+++ src/main/java/dto/CreateClassInfoDto.java
@@ -0,0 +1,24 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import java.time.LocalTime;
+
+@SuperBuilder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class CreateClassInfoDto {
+    private Integer classID;
+    private String className;
+    private Integer classDuration;
+    private LocalTime startTime;
+    private LocalTime endTime;
+//    private LocalDateTime dateClassStart;
+//    private LocalDateTime dateClassEnd;
+}
